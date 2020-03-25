f4715947c84b0cbec2ea01d0df108dcfadb4ba83
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2020-03-25 00:09:02 -0400

    VueJs consumiendo api de rickymorty

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,138 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+      <div class="hero is-white is-gradient is-bold">
+        <div class="hero-body">
+          <h1 class="title">
+            <span class="has-text-success">R&M</span>
+            <span class="subtitle">Personajes</span>
+          </h1>
+          <div class="field has-addons is-pulled-right">
+
+            <div class="control">
+              <input v-model="search" type="text" class="input is-rounded" @keyup.enter="searchData">
+            </div>
+
+            <div class="control">
+              <button class="button is-success is-rounded" @click="searchData">Buscar</button>
+            </div>
+
+          </div>
+        </div>
+      </div>      
+
+      <div class="container">
+        <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+          <Character @showModal="showModal" v-for="character in characters" :key="character.id" v-bind:character="character"/>
+        </div>
+        <nav class="pagination" role="navegation" aria-label="pagination">
+          <a class="pagination-previous" @click="changePage( page - 1)">Anterior</a>
+            <ul class="pagination-list">
+              <li>
+                <a class="pagination-link is-current">{{ page }}</a>
+              </li>
+            </ul>
+          <a class="pagination-next" @click="changePage(page+1)">Siguiente</a>
+        </nav>
+      </div>  
+
+      <div class="modal" :class="{ 'is-active': modal }" v-if="modal">
+        <div class="modal-background" @click="modal=false" ></div>  
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">
+              Acerca de: {{ currentCharacter.name }}
+            </p>
+          </header>
+
+          <div class="modal-card-body">
+            <p>Genero: </p>
+            <strong>{{ currentCharacter.gender }} </strong>
+            <p>Estatus: </p>
+            <strong>{{ currentCharacter.status }} </strong>
+            <p>Especie: </p>
+            <strong>{{ currentCharacter.species }} </strong>
+            <p>Tipo: </p>
+            <strong>{{ currentCharacter.type }} </strong>
+          </div>
+
+          <footer class="modal-card-foot">
+            <button class="button" @click="modal=false">
+              Cerrar
+            </button>
+          </footer>
+
+        </div>
+
+      </div>    
+
+    </div>    
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import axios from 'axios';
+import Character from './components/Character'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name:'App',
+  components:{
+    Character
+  },
+  data() {
+    return {
+      characters: [],
+      page: 1,
+      pages: 1,
+      search: '',
+      modal: false,
+      currentCharacter: {}
     }
+  },
+  methods: {
+    fetch() {
+
+      const params = {
+        page: this.page,
+        name: this.search
+      }
+
+      let result = axios.get("https://rickandmortyapi.com/api/character", { params })
+                    .then((response) => {
+                      this.characters = response.data.results;
+                      this.pages = response.data.info.pages;
+                      
+                    })
+                    .catch((error) => {
+                      console.log(error)
+                    })
+    },
+    changePage(page) {
+      this.page = page <= 0 || page > this.pages ? this.page : page;
+      this.fetch();
+    },
+    searchData() {
+      this.page = 1;
+      this.fetch();
+    },
+    showModal (id) {
+      this.fetchOne(id)
+    },
+    async fetchOne (id) {
+      let result = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+      this.currentCharacter = result.data
+      this.modal = true
+  
+    }
+  },
+  created() {
+    this.fetch();
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
---
+++ src/components/Character.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="column is-12-mobile is-4-desktop is-4-tablet">
+        <div class="card">
+            <div class="card-header">
+                <img v-bind:src="character.image" alt="">
+            </div>
+            <div class="content">
+                <h3 class="title is-size-4">{{ character.name }}</h3>
+                <button class="button is-success is-rounded is-small" @click="showMore(character.id)">Ver mas</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["character"],
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            showMore(id) {
+                this.$emit('showModal', id);
+            }
+        },
+        created() {
+
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
